41a0c62488944e00e49ff6ad1104f8a0ebf42365
rcburnet <rcburnet@github.com>
2016-06-22 16:04:50 -0400

    added scripts for local sky subtraction as detailed in Balogh's email on June 15.

---
+++ diagnostic_of_objects/local_sky_subtraction/modules.py
@@ -0,0 +1,11 @@
+##All the modules to be used by the scripts
+
+import numpy as np
+import scipy as sp
+import glob
+import os
+import fnmatch
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from matplotlib import rc
+from astropy.io import fits
