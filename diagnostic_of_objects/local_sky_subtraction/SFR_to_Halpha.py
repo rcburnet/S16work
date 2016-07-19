7b5719d6353160dab3a6ef5af7135e15d33771e3
rcburnet <rcburnet@github.com>
2016-07-19 16:14:02 -0400

    added script to calculated expected Halpha flux levels from SFR as noted in Sean's catalogue txt tables

---
+++ diagnostic_of_objects/local_sky_subtraction/SFR_to_Halpha.py
@@ -0,0 +1,3 @@
+# Script that will take SFR(OII) values from Seans catalogue (txt tables) and convert it to Halpha flux using K98 relation (SFR = 7.9*10e-42*L(Halpha), L(Halpha) = 4*pi*D**2*Flux(Halpha)).
+
+
