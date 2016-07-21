2f2f3459ff34e70e6de934249cf816877d6bfbab
rcburnet <rcburnet@github.com>
2016-07-21 13:54:29 -0400

    updated comments

--- diagnostic_of_objects/local_sky_subtraction/SFR_to_Halpha.py
+++ diagnostic_of_objects/local_sky_subtraction/SFR_to_Halpha.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-# Script that will take SFR(OII) values from Seans catalogue (txt tables) and convert it to Halpha flux using K98 relation (SFR = 7.9*10e-42*L(Halpha), L(Halpha) = 4*pi*D**2*Flux(Halpha)).
+# Script that will take SFR(OII) values from Seans catalogue (txt tables) and convert it to Halpha flux using K98 relation (SFR = 7.9*10e-42*L(Halpha), L(Halpha) = 4*pi*D**2*Flux(Halpha)). Does it for both corrected SFR(OII) values and uncorrected values.
 
 import numpy as np
 from astropy.cosmology import FlatLambdaCDM
